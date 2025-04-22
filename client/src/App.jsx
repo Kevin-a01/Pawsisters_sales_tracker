4c81d6f294d90f1fce30e31d0eb226beab98f755
Kevin Andersson <kevin200032@live.se>
2025-04-22 12:23:28 +0200

    fix toggle darkmode button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,13 @@ import {BrowserRouter as Router , Routes, Route} from "react-router-dom";
 import AddProduct from "./pages/AddProduct";
 import HomePage from "./HomePage";
 import SalesDetail from "./pages/SalesDetail";
+import ToggleTheme from "./components/ToggleTheme";
 
 function App() {    
   return (
+    
     <Router>
+      <ToggleTheme/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/add-product" element={<AddProduct/>}/>
