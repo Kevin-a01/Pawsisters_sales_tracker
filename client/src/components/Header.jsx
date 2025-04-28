a79098960c31f75db5aff9451bb90c88001a0675
Kevin Andersson <kevin200032@live.se>
2025-04-28 22:31:45 +0200

    fix password and some ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,8 +4,8 @@ function Header() {
 
   return(
     <div className="w-full mx-auto p-6 bg-pink-300 shadow-md flex justify-center items-center">
-    <Link to="/" className="">
-    <h1 className="text-4xl font-bold mb-6 text-center mt-1 hover:text-pink-600 hover:ease-in hover:duration-200 ">Pawsisters Sales Tracker</h1>
+    <Link to="/" className="mb-6">
+    <h1 className="text-4xl font-bold text-center mt-1 hover:text-pink-600 hover:ease-in hover:duration-200 ">Pawsisters Sales Tracker</h1>
     </Link>
     </div>
 
