e4944811c9c6f8b1aa1eec059d7c5d616258ceaa
Kevin Andersson <kevin200032@live.se>
2025-06-15 21:40:57 +0200

    fix tasklist for calendar details

---
+++ server/routes/tasks.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../db');
+
+router.get('/:date', async (req, res) => {
+    const {date} = req.params;
+    try{
+        const result = await pool.query(`
+            SELECT * FROM tasks WHERE date = $1
+            `,[date]
+        );
+        res.status(200).json({ task:result.rows});
+
+    }catch(err){
+        console.error("Error fetching tasks", err);
+        res.status(500).json({error: "Internal server error"});
+        
+
+    }
+
+});
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    const {completed} = req.body;
+
+    try{
+        const result = await pool.query(
+            `UPDATE tasks SET completed = $1 WHERE task_id = $2 RETURNING * `, [completed, id]
+        );
+
+        res.status(200).json({task:result.rows[0]});
+
+    }catch(err){
+        console.error("Error updating task", err);
+        res.status(500).json({error: "Internal server error."});
+        
+
+    }
+
+})
+
+router.delete('/:date', async (req, res) => {
+
+    const {date} = req.params;
+    try{
+        const result = await pool.query(`
+            DELETE FROM tasks WHERE date = $1
+            `, [date]);
+
+        res.status(200).json({task:result.rows});
+
+    }catch(err){
+        console.error("Error deleting tasks", err);
+        res.status(500).json({error: "Internal server error."});
+    }
+});
+
+
+router.post('/', async  (req, res) => {
+
+    try{
+        const {task, date} = req.body
+
+
+        if(!task || !date){
+            return res.status(400).json({Error: "Missing required fields"});
+        }
+
+       const result = await pool.query(`
+            INSERT INTO tasks (task, date) VALUES($1, $2)RETURNING *
+            
+            `, [task, date]
+        );
+        res.status(200).json({message: "Uppgift sparad", task: result.rows[0]});
+    }catch(err){
+        console.error("Error saving task", err);
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
+module.exports = router;
