77aabad495a429e33cf9e551fd614fdd9934b699
Kevin Andersson <kevin200032@live.se>
2025-06-17 19:46:14 +0200

    fix PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const isProduction = process.env.NODE_ENV === "production"
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: "autoUpdate",
+    includeAssets: ['favicon/favicon.ico', 'favicon/apple-touch-icon.png'],
+    manifest: {
+      name: "Pawsisters Sales Tracker",
+      short_name: "Pawsisters ST",
+      start_url: '/',
+      display: "standalone",
+      background_color: '#ffffff',
+      theme_color: "#ff69b4",
+      icons: [
+        {
+          src: '/favicon/web-manifest-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/favicon/web-manifest-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+
+      ]
+
+    }
+
+  })],
   server: {
     host: true,
     port: 3000,
